a35701b43540805bd7163d0cc397b7cbd1d9a98f
Chenkowong <chenko@Chenkowongs-MacBook-Pro-2.local>
2019-12-28 15:44:08 +0800

    remove test

--- ios/Classes/FlutterPluginBatterylevelPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterPluginBatterylevelPlugin.h"
-#import <flutter_plugin_batterylevel/flutter_plugin_batterylevel-Swift.h>
-
-@implementation FlutterPluginBatterylevelPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterPluginBatterylevelPlugin registerWithRegistrar:registrar];
-}
-@end
